d9ad8926c3ad0097f9456fa4c1b1020af4e539d6
Andrei Martynenko <139020977+AxinitM@users.noreply.github.com>
2024-12-22 11:29:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
    if (bmi < 18.5) {
       bmiResultConclusion.textContent =
-        "If your BMI is below 18.5, you are considered underweight. It's important to consult a healthcare professional to understand the reasons behind being underweight and develop a plan to achieve a healthier weight. Focus on eating a balanced diet rich in nutrient-dense foods like lean proteins, whole grains, and healthy fats. Gradually increasing your caloric intake and including resistance exercises can also help promote muscle gain.";
+        "If your BMI is below 18.50, you are considered underweight. It's important to consult a healthcare professional to understand the reasons behind being underweight and develop a plan to achieve a healthier weight. Focus on eating a balanced diet rich in nutrient-dense foods like lean proteins, whole grains, and healthy fats. Gradually increasing your caloric intake and including resistance exercises can also help promote muscle gain.";
       bmiResultContainer.style.backgroundColor = "var(--resultBadBgColor)";
     } else if (bmi >= 18.5 && bmi <= 24.99) {
       bmiResultConclusion.textContent =
@@ -97,11 +97,11 @@ document.addEventListener("DOMContentLoaded", function () {
       bmiResultContainer.style.backgroundColor = "var(--resultGreatBgColor)";
     } else if (bmi > 24.99 && bmi < 30) {
       bmiResultConclusion.textContent =
-        "If your BMI falls between 25 and 29.99, you are considered overweight. To reduce your weight, focus on maintaining a calorie deficit by consuming fewer calories than you burn. Opt for a balanced diet with more vegetables, fruits, lean proteins, and whole grains while minimizing high-calorie processed foods. Increase your physical activity by incorporating aerobic exercises like brisk walking, cycling, or swimming for at least 30 minutes a day, five days a week.";
+        "If your BMI falls between 25.00 and 29.99, you are considered overweight. To reduce your weight, focus on maintaining a calorie deficit by consuming fewer calories than you burn. Opt for a balanced diet with more vegetables, fruits, lean proteins, and whole grains while minimizing high-calorie processed foods. Increase your physical activity by incorporating aerobic exercises like brisk walking, cycling, or swimming for at least 30 minutes a day, five days a week.";
       bmiResultContainer.style.backgroundColor = "var(--resultBadBgColor)";
     } else if (bmi >= 30) {
       bmiResultConclusion.textContent =
-        "A BMI of 30 or higher is classified as obese. Obesity can increase your risk of developing chronic health issues, such as type 2 diabetes, heart disease, and certain cancers. Work with a healthcare provider to create a weight-loss plan tailored to your needs. Focus on a healthy eating pattern that emphasizes vegetables, fruits, lean proteins, and whole grains. Reducing portion sizes and cutting back on sugary and high-fat foods can also be beneficial. Aim for at least 150 minutes of moderate physical activity each week, including both aerobic exercises and strength training.";
+        "A BMI of 30.00 or higher is classified as obese. Obesity can increase your risk of developing chronic health issues, such as type 2 diabetes, heart disease, and certain cancers. Work with a healthcare provider to create a weight-loss plan tailored to your needs. Focus on a healthy eating pattern that emphasizes vegetables, fruits, lean proteins, and whole grains. Reducing portion sizes and cutting back on sugary and high-fat foods can also be beneficial. Aim for at least 150 minutes of moderate physical activity each week, including both aerobic exercises and strength training.";
       bmiResultContainer.style.backgroundColor = "var(--resultWorseBgColor)";
     }
   }
